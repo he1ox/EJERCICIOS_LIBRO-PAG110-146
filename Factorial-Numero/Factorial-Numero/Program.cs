dcbd704f964b6291eaf2959e03b20a5e7bda8d58
Jorge A. Lopez <68780219+he1ox@users.noreply.github.com>
2021-02-27 09:29:23 -0600

    Add files via upload

---
+++ Factorial-Numero/Factorial-Numero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, acum = 1;
+
+            do
+            {
+                Console.Write("Ingrese un numero para calcular el factorial: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            } while (num < 0 || num == 0);
+            
+
+            for(int i = 1; i <= num; i++)
+            {
+                acum *= i;
+            }
+
+            Console.WriteLine(acum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerieNumero/SerieNumero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, lim, i, aux;
+            Console.WriteLine("Ingrese el número a calcular la sucesion Fibonacci: ");
+
+            lim = int.Parse(Console.ReadLine());
+
+            a = 0; b = 1;
+
+            for (i = 0; i < lim; i++)
+            {
+                aux = a;
+                a = b;
+                b = aux + a;
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
